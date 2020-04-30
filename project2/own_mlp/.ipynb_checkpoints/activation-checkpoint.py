29423883c8684ce3926541f485e898be41cb7e4e
Sylvain Lugeon <sylvain.lugeon@epfl.ch>
2020-04-30 19:21:03 +0200

    separate into different files

---
+++ project2/own_mlp/.ipynb_checkpoints/activation-checkpoint.py
@@ -0,0 +1,44 @@
+import torch
+import random
+import math
+from module import *
+
+# Sigmoid activation 
+class Sigmoid(Module):
+    def __init__(self):
+        self.in_value = None
+
+    def forward(self, *input):
+        x = 1 / (1 + (-input[0]).exp())
+        self.in_value = x
+        return x
+    
+    def derivative(self, v):
+        return self.forward(v) * self.forward(-v) # fancy form for derivative
+    
+    def backward(self, *gradwrtoutput):
+        return self.derivative(self.in_value) * gradwrtoutput[0] # elementwise multiplication
+    
+    def to_string(self): return "Sigmoid"
+    
+# ReLU activation
+class Relu(Module):
+    def __init__(self):
+        self.in_value = None
+
+    def forward(self, *input):
+        x = input[0].clone()
+        x[x < 0] = 0
+        self.in_value = x
+        return x
+    
+    def derivative(self, v):
+        vv = v.clone()
+        vv[vv <= 0] = 0
+        vv[vv > 0] = 1
+        return vv
+    
+    def backward(self, *gradwrtoutput):
+        return self.derivative(self.in_value) * gradwrtoutput[0] # elementwise multiplication
+    
+    def to_string(self): return "ReLU"
---
+++ project2/own_mlp/.ipynb_checkpoints/linear-checkpoint.py
@@ -0,0 +1,55 @@
+import torch
+import random
+import math
+from module import *
+
+# Linear module
+class Linear(Module):
+    def __init__(self, in_size, out_size):
+        
+        self.in_size = in_size
+        self.out_size = out_size
+        
+        # initializing the weights
+        stdv = 1. / math.sqrt(self.in_size)
+        self.w = torch.empty(out_size, in_size).uniform_(-stdv, stdv)
+        self.b = torch.empty(out_size).uniform_(-stdv, stdv)
+        
+        # initializing the gradients
+        self.dw = torch.empty(out_size, in_size).zero_()
+        self.db = torch.empty(out_size).zero_()
+        
+        self.in_value = torch.empty(in_size)
+    
+    # only with one input sample
+    def forward(self, *input):
+        self.in_value = input[0]
+        return self.w.mv(input[0]) + self.b
+    
+    def backward(self, *gradwrtoutput):
+        
+        # gradient for that one sample
+        dw_c = gradwrtoutput[0].view(-1, 1).mm(self.in_value.view(1, -1))
+        db_c = gradwrtoutput[0]
+        
+        # updating stored gradients
+        self.dw.add_(dw_c)
+        self.db.add_(db_c)
+        
+        # gradient with respect to the input
+        d_input = gradwrtoutput[0].view(1, -1).mm(self.w)
+        
+        return d_input[0]
+    
+    def param(self):
+        return self.w, self.b, self.dw, self.db
+    
+    # updating the weights wrt to the step size and gradients stored, then set the gradients back to zero
+    def update_param(self, step_size):
+        self.w = self.w - step_size * self.dw
+        self.b = self.b - step_size * self.db
+        self.dw.zero_()
+        self.db.zero_()
+        
+    def to_string(self):
+        return "Linear ({}, {})".format(self.in_size, self.out_size)
