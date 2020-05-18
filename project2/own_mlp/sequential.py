5a888639e69973989ca6c6072c6d1ce4d16c160d
Tyler Benkley <tylerbenkley@tylers-macbook-pro.home>
2020-05-18 19:19:47 +0200

    Adding instance testing in Sequential initialization

--- project2/own_mlp/sequential.py
+++ project2/own_mlp/sequential.py
@@ -10,11 +10,28 @@ from activation import *
 
 # A sequence of modules
 class Sequential(Updatable):
-    def __init__(self, *modules, loss=None):
+    def __init__(self, *modules):
         super(Sequential, self).__init__()
-        self.layers = modules
-        self.loss = loss
-        
+        self.layers = []
+        for module in modules:
+            if isinstance(module,Cell):
+                self.layers.append(module)
+            elif isinstance(module,Sequential):
+                for layer in module.layers:
+                    self.layers.append(layer)
+            else:
+                raise NotImplementedError("Invalid layer input")
+                
+    def add_layers(self,*modules):
+        for module in modules:
+            if isinstance(module,Cell):
+                self.layers.append(module)
+            elif isinstance(module,Sequential):
+                for layer in module.layers:
+                    self.layers.append(layer)
+            else:
+                raise NotImplementedError("Invalid layer input")
+    
     def forward(self, input):
         x = input
         for m in self.layers: # not forwarding through the loss
@@ -31,7 +48,6 @@ class Sequential(Updatable):
         for m in self.layers:
             if isinstance(m, Updatable):
                 m.update_param(step_size)
-    
     
     def train(self, input, target, epochs, batch_size = 10, verbose=True):
         
@@ -70,8 +86,6 @@ class Sequential(Updatable):
             
     def describe(self):
         print(self.to_string())
-        if self.loss is not None:
-            print(self.loss.to_string())
         
     def to_string(self):
         s = ''
